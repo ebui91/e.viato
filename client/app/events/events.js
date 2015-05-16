911a8997ae6909fc89231a0497be123207526bd4
Zach Royer <zroyer@gmail.com>
2015-05-15 19:34:20 -0500

    autocomplete removed again for aesthetics... filter does the trick just fine now

--- client/app/events/events.js
+++ client/app/events/events.js
@@ -118,15 +118,3 @@ angular.module('lunchCorgi.events', [])
     }
   }
 })
-.directive('autocomplete', function($timeout) {
-  return function($scope, element, attribute) {
-    element.autocomplete({
-      source: $scope[attribute.uiItems],
-      select: function() {
-        $timeout(function() {
-          element.trigger('input');
-        }, 0)
-      }
-    });
-  };
-})
